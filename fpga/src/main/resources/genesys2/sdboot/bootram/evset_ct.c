90fe38de6d5b0406289dd82ec9d9c7b5b958d409
XueZihan <xuezihan1992@outlook.com>
2022-07-09 17:55:31 +0800

    faster ct

--- fpga/src/main/resources/genesys2/sdboot/bootram/evset_ct.c
+++ fpga/src/main/resources/genesys2/sdboot/bootram/evset_ct.c
@@ -7,24 +7,52 @@
 #include "atdect.h"
 
 
-cacheBlock_t*    target;
+cacheBlock_t*    target[2];
 cacheBlock_t*    probe;
 uint32_t   evsize;
 uint8_t    configs;
-uint16_t   fails[2] ;
+uint16_t   fails[3] ;
 uint8_t    speed;
-uint32_t   accesses[2] ;
+uint32_t   accesses[3] ;
 uint32_t   i;
+uint32_t   j;
 
-void prepare(uint8_t speed, uint8_t casize, cacheBlock_t** evtarget) { //fill congruent addresses
+inline void prepare(uint8_t speed, uint8_t evsize, cacheBlock_t** evtarget) { //fill congruent addresses
+  if(speed < 2) {
+    *(evtarget + 1) = *evtarget;
     access((void *)(*evtarget));
-   if(speed == 1) { for(uint8_t i = 0; i<casize; i++) access((void *)(evset[i])); }
+    if(speed == 1) { for(j = 0; j<evsize; j++) access((void *)(evset[j])); }
+  } else {
+    switch(evsize) {
+      case 0x00  : accessWithfence((void *)(*evtarget)); *(evtarget + 1) = *evtarget ; break;  //A 0
+      case 0x01  :                                     ; *(evtarget + 1) =  evset[0] ; break;  //B 1
+      case 0x02  : accessWithfence((void *)(*evtarget)); *(evtarget + 1) =  evset[1] ; break;  //C 2
+      case 0x03  :                                     ; *(evtarget + 1) = *evtarget ; break;  //D 3
+      case 0x04  : accessWithfence((void *)(evset[0])) ; *(evtarget + 1) =  evset[2] ; break;  //E 4
+      case 0x05  :                                     ; *(evtarget + 1) =  evset[3] ; break;  //F 5
+      case 0x06  :                                     ; *(evtarget + 1) =  evset[0] ; break;  //G 6
+      case 0x07  : accessWithfence((void *)(evset[1])) ;
+                   accessWithfence((void *)(*evtarget)); *(evtarget + 1) =  evset[4] ; break;  //H 7
+      case 0x08  :                                     ; *(evtarget + 1) =  evset[5] ; break;  //I 8
+      case 0x09  :                                     ; *(evtarget + 1) =  evset[6] ; break;  //J 9
+      case 0x0a  :                                     ; *(evtarget + 1) =  evset[1] ; break;  //K 10
+      case 0x0b  : accessWithfence((void *)(evset[2])) ;
+                   accessWithfence((void *)(evset[3])) ;
+                   accessWithfence((void *)(evset[0])) ;
+                   accessWithfence((void *)(*evtarget)); *(evtarget + 1) =  evset[7] ; break;  //L 11
+      case 0x0c  :                                     ; *(evtarget + 1) =  evset[8] ; break;  //M 12
+      case 0x0d  :                                     ; *(evtarget + 1) =  evset[9] ; break;  //N 13
+      case 0x0e  :                                     ; *(evtarget + 1) =  evset[10]; break;  //O 14
+      case 0x0f  :                                     ; *(evtarget + 1) =  evset[2] ; break;  //P 15
+      default    :                                     ; *(evtarget + 1) = *evtarget ; break;
+    }
+  }
 }
 
 int main() {
   uart_init();
   kprintln("evset_ct in lru cache TESTS %d", TESTS);
-  kprintln("config  slow fails aver_accesses fast fails aver_accesses");
+  kprintln("config  slow fails aver_acc fast fails aver_acc faster fails aver_acc");
 
   speed = 0;
 
@@ -48,24 +76,31 @@ int main() {
     probe = (cacheBlock_t*)TEST_START + (100*BLKS);
     for(i = 0; i < 10*BLKS; i++) access((void *)probe++);
 
-    fails[0]    = 0;
-    fails[1]    = 0;
-    accesses[0] = 0;
-    accesses[1] = 0;
-    for(uint16_t tests = 0, speed = 0; tests < TESTS * 2; tests++) {
-      if(tests >= TESTS) speed = 1;
-      target      = (cacheBlock_t*)TEST_TARGET;// + tests;
+    fails[0]    = 0; fails[1]    = 0; fails[2]    = 0;
+    accesses[0] = 0; accesses[1] = 0; accesses[2] = 0;
+    for(uint16_t tests = 0, speed = 0; tests < TESTS * 3; tests++) {
+      if(tests >=   TESTS) speed = 1;
+      if(tests >= 2*TESTS) speed = 2;
+      target[0]   = (cacheBlock_t*)TEST_TARGET;// + tests;
       probe       = (cacheBlock_t*)TEST_START;
       for(evsize = 0; evsize < WAYS; ) {
-        for(prepare(speed, evsize, &target); clcheck_f((void *)target); access((void *)probe++)) accesses[speed] ++;
+        for(prepare(speed, evsize, target); clcheck_f((void *)target[1]); access((void *)probe++)) accesses[speed] ++;
+        accessWithfence((void *)target[1]);
         evset[evsize++] = probe - 1;
       }
+
       //check
-      if(evset_test((void *)target, 1) < 1) fails[speed] ++;
+      //in speed 2 before test drain out first
+      if(speed == 2) for(access((void *)target[0]); clcheck_f((void *)target[0]); access((void *)probe++), accesses[speed] ++);
+      if(evset_test((void *)target[0], 2) < 2) fails[speed] ++;
     }
-    //kprintln("config  slow fails aver_accesses fast fails aver_accesses");
-    kprintln("   %d           %d      %d           %d      %d ", 
-              configs,  fails[0], accesses[0]/TESTS, fails[1], accesses[1]/TESTS);
+
+    accesses[0] = accesses[0]/TESTS;
+    accesses[1] = accesses[1]/TESTS;
+    accesses[2] = accesses[2]/TESTS;
+    //kprintln("config  slow fails aver_acc fast fails aver_acc faster fails aver_acc");
+    kprintln(" %d             %d      %d          %d      %d            %d       %d",
+              configs,  fails[0], accesses[0], fails[1], accesses[1], fails[2], accesses[2]);
   }
   while(1);
 }
