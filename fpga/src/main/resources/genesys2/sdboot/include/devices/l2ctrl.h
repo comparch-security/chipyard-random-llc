d88d07463f1e129f1a58b0d1c65e86567059fe0c
XueZihan <xuezihan1992@outlook.com>
2022-07-11 17:09:04 +0800

    add l2ctrl.h

---
+++ fpga/src/main/resources/genesys2/sdboot/include/devices/l2ctrl.h
@@ -0,0 +1,16 @@
+// See LICENSE.Sifive for license details.
+
+#ifndef _SIFIVE_L2CTRL_H
+#define _SIFIVE_L2CTRL_H
+
+#define L2_INFORMATIONS        (0x000)
+#define L2_FLUSH64             (0x200)
+#define L2_FLUSH32             (0x240)
+#define L2_BLKSTATE            (0x250)
+#define L2_ATDECT_CONFIG0      (0x2C0)
+#define L2_ATDECT_CONFIG1      (0x2C8)
+
+#define L2_CMDBITS             8
+
+
+#endif /* _SIFIVE_GPIO_H */
