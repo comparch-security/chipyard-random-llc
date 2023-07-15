4c657d0d0e013d79ee68fec8c26c0398ff9614bc
XueZihan <xuezihan1992@outlook.com>
2023-07-15 11:06:35 +0800

    Scala Compilation Breaks Due to FIRRTL -> Treadle Incompatability https://github.com/firesim/firesim/issues/888

--- variables.mk
+++ variables.mk
@@ -155,6 +155,7 @@ JAVA_OPTS ?= -Xmx$(JAVA_HEAP_SIZE) -Xss8M -XX:MaxPermSize=256M
 #########################################################################################
 # by default build chisel3/firrtl and other subprojects from source
 override SBT_OPTS += -Dsbt.sourcemode=true -Dsbt.workspace=$(base_dir)/tools
+override SBT_OPTS += -DfirrtlVersion=1.4.1
 
 SCALA_BUILDTOOL_DEPS = $(SBT_SOURCES)
 
